39e81b1d9c2f4436a01a70faa95148950c549a57
izikpenso <izikp@radware.com>
2014-02-02 16:27:56 +0200

    Adding editing the tempest.conf before run

--- run_tempest_tests.sh
+++ run_tempest_tests.sh
@@ -26,6 +26,10 @@ fi
 #source the jobrc file that contains all the needed environment variables for script to run. 
 source $1
 
+
+#Editing tempest.conf with radware timeout
+sshpass -p $VM_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $VM_SSH_USER@$VM_IP 'sed -i "s/#lbaas_entity_delete_timeout=10/lbaas_entity_delete_timeout=1000/g" /opt/stack/tempest/etc/tempest.conf'
+
 #Runnuig tempest test and save log
 sshpass -p $VM_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $VM_SSH_USER@$VM_IP 'nosetests -s -v /opt/stack/tempest/tempest/api/network/radware_test_load_balancer.py --with-xunit --xunit-file=test_load_balancer_log.xml'
 
