7879d4e096d66c0eda3ad7b561f8e2d45a27b204
izikpenso <izikp@radware.com>
2014-03-26 19:19:30 +0200

    Changing log name when gerrit is triggerd

--- publish_on_dropbox.sh
+++ publish_on_dropbox.sh
@@ -13,8 +13,6 @@ if (( $# != 1 )); then
 fi
 
 
-TIMESTAMP=$(date -d "today" +"%Y%m%d%H%M")
-
 #source the jobrc file that contains all the needed environment variables for script to run. 
 source $1
 
@@ -22,11 +20,10 @@ source $1
 
 if [ -z "$GERRIT_REFSPEC" ]; then
       echo "VM_NAME: $VM_NAME"
-      LOG_FILE_NAME="${VM_NAME}_${TIMESTAMP}.tar.gz"
+      LOG_FILE_NAME="${VM_NAME}_${BUILD_ID}.tar.gz"
 else
       echo "GERRIT_REFSPEC is: $GERRIT_REFSPEC"
-      PATCH_NAME=$(echo $GERRIT_REFSPEC | sed -e 's/\//_/g')
-      LOG_FILE_NAME="${PATCH_NAME}_${TIMESTAMP}.tar.gz"
+      LOG_FILE_NAME="${GERRIT_CHANGE_NUMBER}_${GERRIT_PATCHSET_NUMBER}_${BUILD_ID}.tar.gz"
 fi
 
 tar cvzf $LOG_FILE_NAME tempest.log test_load_balancer_log.xml screen-logs.tar.gz
