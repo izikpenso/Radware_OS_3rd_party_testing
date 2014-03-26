b0ed333da996be6af63592dc5739e117ae8da0e3
izikpenso <izikp@radware.com>
2014-03-26 12:31:16 +0200

    Fix if

--- publish_on_dropbox.sh
+++ publish_on_dropbox.sh
@@ -20,13 +20,19 @@ source $1
 
  ~/bin/dropbox.py start
 
-if [ -z "$GERRIT_REFSPEC" ]; then 
+if [ -z "$GERRIT_REFSPEC" ]; then
+      
+      echo "inside 1" 
       LOG_FILE_NAME=$VM_NAME_$timestamp.tar.gz
 else
+      echo "inside 2" 
       PATCH_NAME=$(echo $GERRIT_REFSPEC | sed -e 's/\//_/g')
       LOG_FILE_NAME=$PATCH_NAME_$timestamp.tar.gz
 fi
 
 tar cvzf $LOG_FILE_NAME.tar.gz tempest.log test_load_balancer_log.xml screen-logs.tar.gz
 cp $LOG_FILE_NAME.tar.gz ~/Dropbox/Public/.
-echo "export LOG_FILE_NAME=$LOG_FILE_NAME$timestamp.tar.gz" >> $1
+
+echo "Log file name is: $LOG_FILE_NAME"
+
+echo "export LOG_FILE_NAME=$LOG_FILE_NAME.tar.gz" >> $1
