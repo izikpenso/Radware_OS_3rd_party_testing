0713e6fc73c9bed4c8df8a55ff59b6f4c6714ffc
izikpenso <izikp@radware.com>
2014-04-13 16:48:13 +0300

    Add stack.sh.log to log file

--- publish_on_dropbox.sh
+++ publish_on_dropbox.sh
@@ -28,7 +28,8 @@ fi
 
 cp /var/lib/jenkins/jobs/$JOB_NAME/builds/$BUILD_NUMBER/log console.log
 
-tar cvzf $LOG_FILE_NAME tempest.log test_load_balancer_log.xml screen-logs.tar.gz console.log jobrc_modified q_log.txt
+tar cvzf $LOG_FILE_NAME tempest.log test_load_balancer_log.xml screen-logs.tar.gz console.log jobrc_modified q_log.txt stack.sh.log localrc vdirect_history.log
+
 cp $LOG_FILE_NAME ~/Dropbox/Public/.
 
 echo "Log file name is: $LOG_FILE_NAME"
