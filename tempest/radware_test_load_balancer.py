80a0d326b2a39a74705a62ccfe529f694bbff741
izikpenso <izikp@radware.com>
2014-09-01 14:21:40 +0300

    Change to support radware user

--- tempest/radware_test_load_balancer.py
+++ tempest/radware_test_load_balancer.py
@@ -45,7 +45,7 @@ class RadwareLoadBalancerTest(test_load_balancer.LoadBalancerTestJSON):
         cls.vips = []
 
         import vdirect_cfg.lib.vdirect_client as VD
-        config = VD.load_config('/home/ubuntu/scripts/vdirect_cfg/test.cfg')
+        config = VD.load_config('/home/radware/scripts/vdirect_cfg/test.cfg')
         rest_client = VD.vDirectClient(server=config['vdirect_ip'],
                                        user=config['vdirect_user'],
                                        password=config['vdirect_password'])
