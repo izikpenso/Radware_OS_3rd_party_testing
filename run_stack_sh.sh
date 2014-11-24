35aa754917c057f299a6c9213901c22b236a7c66
izikpenso <izikp@radware.com>
2014-11-24 13:49:53 +0200

    BUG fix - wourkaround to stack.sh hanging in the end

--- run_stack_sh.sh
+++ run_stack_sh.sh
@@ -28,4 +28,4 @@ fi
 source $1
 
 #Runnuig stack.sh
-sshpass -p $VM_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $VM_SSH_USER@$VM_IP '~/devstack/./stack.sh; exit'
+sshpass -p $VM_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $VM_SSH_USER@$VM_IP '~/devstack/./stack.sh > /dev/null;'
