544c3dcfec382735e0b233497ca0d08b48aa1e26
izikpenso <izikp@radware.com>
2015-03-19 09:32:00 +0200

    Adding files for lbaas v2 support

---
+++ restart_neutron_server_with_radware_lbaas_v2.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -x
+
+source ~/devstack/jobrc
+
+
+if [ -z "$HA_PAIR_FLAG" ]; then
+        HA_PAIR_FLAG=False
+fi
+
+
+sed -i "s/service_provider = LOADBALANCER:[Rr]adware/# service_provider = LOADBALANCER:Radware/g" /etc/neutron/neutron_lbaas.conf
+sed -i "s/# service_provider = LOADBALANCERV2:LoggingNoop/service_provider = LOADBALANCERV2:LoggingNoop/g" /etc/neutron/neutron_lbaas.conf
+
+#sed -i "s/# service_plugins = neutron_lbaas.services.loadbalancer.plugin.LoadBalancerPlugin/service_plugins = neutron_lbaas.services.loadbalancer.plugin.LoadBalancerPluginv2/g" /etc/neutron/neutron.conf
+
+
+#sudo echo '[radwarev2]' | sudo tee -a /etc/neutron/neutron.conf
+#echo "vdirect_address=$VDIRECT_IP" | sudo tee -a /etc/neutron/neutron.conf
+#echo "service_ha_pair=$HA_PAIR_FLAG" | sudo tee -a /etc/neutron/neutron.conf
+
+# stop neutron
+PID=`ps -ef | grep neutron-server | grep python | awk '{ print $2 }'` || true
+echo 'Killing neutron ...PID:' $PID
+
+# kill it
+kill $PID || true
+
+sleep 2
+
+# start neutron
+echo 'Restarting...'
+python /usr/local/bin/neutron-server --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/ml2/ml2_conf.ini > ~/q_err.txt 2> ~/q_log.txt &
+
+
+sleep 2
+
+NEW_PID=`ps -ef | grep neutron-server | grep python| awk '{ print $2 }'`
+echo 'New PID: ' $NEW_PID
---
+++ run_tempest_tests_v2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# This script was should work only in Ubuntu machines.
+
+usage(){
+        echo "Usage: $0 filename ; the file should contain all the necessary environment variables for the script to run. see jobrc_sample file"
+        exit 1
+}
+
+if (( $# != 1 )); then
+   usage
+fi
+
+# sshpass cmd sould be installed fo this script to work, cheking
+if ! which sshpass > /dev/null; then
+   echo -e "sshpass command not found! this package should be installed for this script will be able to run, would you like to install it now? (y/n) \c"
+   read REPLY
+   if [ "$REPLY" == "y" ]; then
+      sudo apt-get --force-yes --yes install sshpass
+   else
+      echo "Script can't run if sshpass package isn't installed, exiting script!"
+      exit 0
+   fi
+fi
+
+#source the jobrc file that contains all the needed environment variables for script to run. 
+source $1
+
+
+#Editing tempest.conf with radware timeout
+
+# Editing - build_timeout, build_interval, allow_tenant_isolation = False
+
+#sshpass -p $VM_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $VM_SSH_USER@$VM_IP 'sed -i "s/#build_timeout = 300/build_timeout = 1200/g" /opt/stack/tempest/etc/tempest.conf; sed -i "s/#build_interval = 1/build_interval = 10/g" /opt/stack/tempest/etc/tempest.conf; sed -i "s/allow_tenant_isolation = True/allow_tenant_isolation = False/g" /opt/stack/tempest/etc/tempest.conf'
+
+#Runnuig tempest test and save log
+#sshpass -p $VM_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $VM_SSH_USER@$VM_IP 'nosetests -s -v /opt/stack/tempest/tempest/api/network/radware_test_load_balancer.py --with-xunit --xunit-file=test_load_balancer_log.xml'
+
+#Run test exclude tests that contains 'health' and 'filter'
+#old cmd
+#sshpass -p $VM_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $VM_SSH_USER@$VM_IP 'nosetests -s -v /opt/stack/tempest/tempest/api/network/radware_test_load_balancer.py --exclude=health --exclude=filter --with-xunit --xunit-file=test_load_balancer_log.xml'
+
+sshpass -p $VM_SSH_PASSWORD ssh -o "StrictHostKeyChecking no" $VM_SSH_USER@$VM_IP 'cd /opt/stack/neutron-lbaas/neutron_lbaas/; sudo tox -r -e tempest '
+
